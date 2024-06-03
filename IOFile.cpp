811995dfb8f34d08c30aa7ab9d3b0a212d95ef71
riqsans <thrq88@gmail.com>
2024-06-03 14:38:25 +0700

    jika file tidak ditemukan akan menampilkan unable to open filie

--- IOFile.cpp
+++ IOFile.cpp
@@ -27,7 +27,24 @@ int main(){
     outfile.close();
 
     //Membuka file dalam mode membaca
-    ifstream infinite;
+    ifstream infile;
     //menunjukkan ke sebuah file
     infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    } 
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 }
