08cd866d9ece38a328e830a87650ee63b9335e1f
riqsans <thrq88@gmail.com>
2024-06-03 15:13:55 +0700

    menambahkan fungsi try

--- ExceptionStandard.cpp
+++ ExceptionStandard.cpp
@@ -4,3 +4,14 @@
 //untuk obyek exception yang akan digunakan
 //untuk obyek array yang akan kita gunakan
 using  namespace std;
+
+int main()
+{
+    cout << "Awal Program " << endl; //penanda 1:...
+    try {
+        array<int, 3> data = { 8, 4, 2};
+        //pesan array integer 3 elemen
+        cout << data.at(5)<< endl;
+    }
+    
+}
