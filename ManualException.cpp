0fbe71b855689bca5453c41b92b3797174f2c08b
riqsans <thrq88@gmail.com>
2024-06-03 15:06:18 +0700

    menuliskan int main program

--- ManualException.cpp
+++ ManualException.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 int main ()
 {
+   try {
+    cout << "Selamat Belajar di Prodi TI UMY " << endl;
+    //throw 0.5; //melemparkakn sebuah integer maka
+    cout << " Pernyataan tidak akan diekseskusi" << endl;
+   }
+   catch (int a) {
+    //blok ini akan dieksekusi
+    cout << "Pengecualian akan dieksekusi" << endl;
+   }
+   catch (...) {
+    /*jika selain integer maka block ini akan dieksekusi*/
+    cout << "default pengecualian dieksekusi" <<  endl;
+   }
    
+    return 0;
 }
