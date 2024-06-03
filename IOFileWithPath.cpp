6a1dc3f68ded34909515a544a443dddbec892182
riqsans <thrq88@gmail.com>
2024-06-03 14:42:59 +0700

    membuka file dalam mode menulis

--- IOFileWithPath.cpp
+++ IOFileWithPath.cpp
@@ -5,4 +5,12 @@ using namespace std;
 
 int main() {
     string baris;
+    string NamaFile;
+
+    cout << "Masukan Nama File : ";
+    cin >> NamaFile;
+
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    
 }
