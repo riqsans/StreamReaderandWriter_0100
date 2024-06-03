28286cfd1a64694c0b97713445c41271e37c220a
riqsans <thrq88@gmail.com>
2024-06-03 14:24:48 +0700

    mendapatkan setiap karakter dalam satu baris

--- IOFile.cpp
+++ IOFile.cpp
@@ -5,8 +5,18 @@ using namespace std;
 
 int main(){
     string baris;
+
     // membuka file dalam mode menulis
     ofstream outfile;
     // menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
 
+    cout << " >= Menulis filr, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while(true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+    }
 }
